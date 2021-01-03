b3dfffbccbe7f763b720d14553f9307be094f5c4
André Puel <andrepuel@gmail.com>
2021-01-03 20:11:50 -0300

    Removed Node-related files

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-let gulp = require('gulp');
-let ts = require('gulp-typescript');
-
-gulp.task('default', function() {
-    return gulp.src("**.ts")
-           .pipe(ts(require('./tsconfig.json').compilerOptions))
-           .pipe(gulp.dest('./ts'));
-});
